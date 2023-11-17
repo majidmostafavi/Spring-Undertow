8a276acc182271ac8dd42f9ef14c45ad6fea6612
Majid Mostafavi <majid@majidmostafavi.com>
2023-11-17 20:50:20 +0330

    First commit

---
+++ src/main/java/com/majidmostafavi/springundertow/SpringUndertowApplication.java
@@ -0,0 +1,13 @@
+package com.majidmostafavi.springundertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUndertowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUndertowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/majidmostafavi/springundertow/SpringUndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majidmostafavi.springundertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUndertowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
